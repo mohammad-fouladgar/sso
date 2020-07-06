6a6dcb2e04c84f5438f3692483d4a2f3f4820a03
mohammad-fouladgar <fouladgar.dev@gmail.com>
2020-07-06 16:21:08 +0430

    wip

---
+++ tests/FirstTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Fouladgar\SSO\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+    /** @test */
+    public function it_can()
+    {
+        $this->assertTrue(true);
+    }
+}
